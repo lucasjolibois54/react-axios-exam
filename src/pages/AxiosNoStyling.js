de3e596e62b80aa313518d210df77f05cf743101
lucasjolibois54 <lucasjolibois@icloud.com>
2023-05-29 12:16:07 +0200

    Update AxiosNoStyling.js

--- src/pages/AxiosNoStyling.js
+++ src/pages/AxiosNoStyling.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import axios, { Axios } from 'axios'
-import {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
